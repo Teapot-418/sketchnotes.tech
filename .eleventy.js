a95836ee37a37463682722ba90f611790dfe6594
Teapot-418 <lischenmoritz@googlemail.com>
2020-09-18 17:43:08 +0200

    Correct link to stylesheet

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("style.css");
     eleventyConfig.addPassthroughCopy("img");
 }
