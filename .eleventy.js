255d4598a4d285d5fbd7ef70bc589e4074cad0d4
Teapot-418 <lischenmoritz@googlemail.com>
2020-11-03 18:18:56 +0100

    Install eleventy with package.json

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,9 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("style.css");
-    eleventyConfig.addPassthroughCopy("img");
-}
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('style.css');
+    eleventyConfig.addPassthroughCopy('img');
+
+    const markdown = require('markdown-it');
+    const markdownAttributes = require('markdown-it-attrs');
+    const markdownLibrary = markdown().use(markdownAttributes);
+    eleventyConfig.setLibrary('md', markdownLibrary);
+};
