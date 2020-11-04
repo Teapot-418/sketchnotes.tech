a4bc94d724ce25c591f6369ee590160ca252666d
Teapot-418 <lischenmoritz@googlemail.com>
2020-11-04 11:41:32 +0100

    Add Youtube-channel

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('style.css');
+    eleventyConfig.addPassthroughCopy('*.css');
     eleventyConfig.addPassthroughCopy('img');
 
     const markdown = require('markdown-it');
